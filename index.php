632cb1ad1d935ec2eeffb9c66ff09a381b3daf5c
RomanKolobanov <54135488+RomanKolobanov@users.noreply.github.com>
2019-09-21 13:01:43 +0300

    0.9

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- metatag -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="autor" content="Layout by Kolobanov">
+    <meta name="keywords" content="Luxury, samp, rp, luxury rp, roleplay, role play, samp server, самп, люксури">
+    
+    <!-- title name -->
+    <title>Luxury RP - SAMP project</title>
+
+    <!-- link -->
+    <link rel="stylesheet" href="/lib/animate.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+    <div class="section sectionone" id="blockone">
+        <div class="sectionone-nav">
+            <div class="nav">
+                <ul class="navbar">
+                    <li class="navbar-list"><a href="#">   Мы в ВКонтакте</a></li>
+                    <li class="navbar-list animated "><a class="text rotate-text" href="#">Донат</a></li>
+                    <li class="navbar-list"><a href="#">О нас</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="sectionone-inner">
+            <h1>Luxury RP</h1>
+            <p>&mdash; надежность и грация!</p>
+        </div>
+        <div class="sectionone-arrow">
+                <a href="#blocktwo"><img class="arrow-down animated fadeInDown slower infinite" width="80" src="/image/lobby/angle-down.svg" alt=""></a>
+        </div>
+    </div>
+    <div class="section sectiontwo" id="blocktwo">
+        <h1>О нас</h1>
+    </div>
+    <div class="section sectionthree" id="blockthree">
+        <h1>Как скачать</h1>
+    </div>
+    <div class="sectionfooter" id="footer">
+        <h1>footer</h1>
+    </div>
+</body>
+</html>
